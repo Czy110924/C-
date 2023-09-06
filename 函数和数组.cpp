4b4891fd7b35200fc9bfb2b6b9b2aeae9552dfd1
Czy110924 <143929283+Czy110924@users.noreply.github.com>
2023-09-06 21:07:54 +0800

    Add files via upload

---
+++ 函数和数组.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int add(int a, int b)
+//{
+//	int z = 0;
+//	z = a + b;
+//	return z;
+//}
+
+//int main()
+//{
+//	int n, m;
+//	scanf("%d %d", &m, &n);
+//	/*int sum = n + m;*/
+//	/*printf("%d", sum);*/
+//	printf("%d", add(m, n));
+//}
+
+//int main()
+//{
+//	//一组相同类型元素的集合
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//char ch[5] = { 'a','b','c'};//abc00 不完全初始化，剩余默认为零
+//	printf("%d", arr[3]);//下标访问数组
+//
+//
+//	return 0;
+//}
---
+++ 初识循环.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int line = 0;
+//	while (line < 300)
+//	{
+//		printf("%d", line);
+//		line++;
+//	}
+//	return 0;
+//}
