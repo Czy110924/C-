3db43af0293d56439006bc15bc3840f8c72e620e
Czy110924 <143929283+Czy110924@users.noreply.github.com>
2023-09-09 22:30:38 +0800

    Add files via upload

--- 双向冒泡排序算法.cpp
+++ 双向冒泡排序算法.cpp
@@ -22,25 +22,27 @@ void change(int* a, int* b)
 int vost(int arr[],int n)
 {
 	int left = 0;
-	int right = n-1;
+	int right = n-1;//数组最后一个数的下标
 	int flag = 0;
 	int i = 0;
 
 	while (left < right)
 	{
 		//left
+		//从左向右运算，大的向右转移
 		for (i = left; i < right-1; i++)
 		{
 			if (arr[i] > arr[i + 1]) {
-				change(&arr[i], &arr[i + 1]);
+				change(&arr[i], &arr[i + 1]);//通过地址交换位置
 			}
-			flag = 1;
+			flag = 1;//确认有交换行为
 		}
-		if (!flag)
+		if (!flag)//有交换行为则继续执行代码，若没有说明已经排序完毕，提前结束循环
 		{
 			break;
 		}
 		//right
+		//从右向左运算，小的向左转移
 		for (i = right; i > left; i--)
 		{
 			if (arr[i] < arr[i - 1])
@@ -48,7 +50,7 @@ int vost(int arr[],int n)
 				change(&arr[i], &arr[i - 1]);
 			}
 		}
-		left++;
+		left++;//交换次数最大 = 数组长度减一
 	}
 	return 0;
 }
