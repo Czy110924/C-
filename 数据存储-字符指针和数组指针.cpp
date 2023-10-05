07f49dcfdb998d2716f29b89f50bc01e85564793
Czy110924 <143929283+Czy110924@users.noreply.github.com>
2023-10-05 20:49:06 +0800

    Add files via upload

---
+++ 数据存储-字符指针和数组指针.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	const char* p = "abcdef";//存的是首字符地址
+//	const char* p1 = "r";
+//	printf("%s\n", p); 
+//	printf("%c\n", p1);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	const char* p1 = "asdf";
+//	const char* p2 = "asdf";
+//
+//	char arr1[] = "asdf";
+//	char arr2[] = "asdf";
+//
+//	if (p1 == p2)
+//	{
+//		printf("p1 == p2");
+//	}
+//	else
+//	{
+//		printf("p1 != p2");
+//	}
+//
+//	if (arr1 == arr2)
+//	{
+//		printf("arr1 == arr2");
+//	}
+//	else
+//	{
+//		printf("arr1 != arr2");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	const char* a = "asdf";
+//	const char* b = "sdfg";
+//	const char* c = "dfgh";
+//
+//	const char* parr[] = { a, b, c };
+//	for (int i = 0; i < 3; i++)
+//	{
+//		printf("%s\n", parr[i]);//当你要打印字符串时，
+//								//printf会根据你传的地址不断打印直到找到\0
+//	}
+//
+//	return 0;
+//}
+
+
+
+int main()
+{
+	
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
